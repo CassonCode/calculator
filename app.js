e1e72351690df6daabf5011165e909361504fefa
Ben Casson <bcasson11@gmail.com>
2021-06-11 15:44:37 -0500

    Resize main elements for mobile

---
+++ app.js
@@ -0,0 +1,28 @@
+const containerPage = document.querySelector(".container-page");
+const containerHistory = document.querySelector(".container-history");
+const historyTextArea = document.querySelector("#history-textarea");
+const mobileHistoryButton = document.querySelector(".mobile-history-button");
+const containerCalculator = document.querySelector(".container-calculator");
+
+containerPage.style.height = window.innerHeight + "px";
+containerPage.style.width = window.innerWidth + "px";
+
+containerCalculator.style.height = window.innerHeight + "px";
+containerCalculator.style.width = window.innerWidth + "px";
+
+containerHistory.style.height = window.innerHeight + "px";
+containerHistory.style.width = window.innerWidth + "px";
+
+historyTextArea.style.height = (window.innerHeight - 50) + "px";
+historyTextArea.style.width = window.innerWidth + "px";
+
+mobileHistoryButton.addEventListener("click", () => {
+    if (containerHistory.classList.contains("mobile-hide-history")) {
+        containerHistory.classList.remove("mobile-hide-history");
+        containerHistory.classList.add("mobile-show-history");
+    }
+    else {
+        containerHistory.classList.add("mobile-hide-history");
+        containerHistory.classList.remove("mobile-show-history");
+    }
+});
