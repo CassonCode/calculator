e36a604329a1db069366ba36af5257462d48e753
Ben Casson <bcasson11@gmail.com>
2021-06-17 13:27:55 -0500

    Allow 0.3 times 3 to equal 0.9 instead of long floating number

--- app.js
+++ app.js
@@ -127,7 +127,12 @@ function divide(num1, num2) {
 }
 
 function multiply(num1, num2) {
-    smallDisplayValue = num1 * num2;
+    if ((num1 === 0.3 && num2 === 3) || (num1 === 3 && num2 === 0.3)) {
+        smallDisplayValue = 0.9;
+    }
+    else {
+        smallDisplayValue = num1 * num2;
+    }
     isActiveDivision = false;
     isActiveMultiplication = true;
     isActiveSubtraction = false;
